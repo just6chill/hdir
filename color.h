da5288fe4fd2dcc2f0c14de69eb3c382edf2e33f
just6chill <54991398+just6chill@users.noreply.github.com>
2021-01-06 12:30:43 +0100

    repo update (new design for help output)

---
+++ color.h
@@ -0,0 +1,8 @@
+#ifndef _DELDIR_H_
+#define _DELDIR_H_
+
+void green(char *text, ...);
+void blue(char *text, ...);
+void red(char *text, ...);
+
+#endif
--- helpf.h
+++ helpf.h
@@ -1,10 +1,6 @@
 #ifndef _HELPF_H_
 #define _HELPF_H_
 
-/*
- * What does it do?
- *	It prints the help page for the program.
- */
-int helpf();
+int helpf(char *args[]);
 
 #endif
