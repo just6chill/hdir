80f57fb2c0d11e62e48b59fabad374912694c841
just6chill <54991398+just6chill@users.noreply.github.com>
2021-02-15 20:01:49 +0100

    some few fixes

--- inc/cerror.h
+++ inc/cerror.h
@@ -2,6 +2,7 @@
 #define _CERROR_H_
 
 const char *code[7];
+enum codes {NO_FILE_DIR, FILE_DIR_EXIST, INVALID_PARAMETER_NUM, DELETE_FILE_DIR_FAIL, CREATE_FILE_DIR_FAIL, COPY_FILE_FAIL, RENAME_FILE_DIR_FAIL};
 int error(const char *code, ...);
 
 #endif
